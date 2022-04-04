0cb0cea6f32882dcd16f6fbcf1ea7342d80c5bdf
SEC4SR <85487197+SEC4SR@users.noreply.github.com>
2022-04-04 14:32:01 +0800

    Update frequency_domain.py

--- defense/frequency_domain.py
+++ defense/frequency_domain.py
@@ -30,7 +30,7 @@ def DS(audio, param=0.5, fs=16000, same_size=True):
     else:
         return new_audio.view(ori_shape[:-1] + new_audio.shape[-1:])
 
-def LPF(new, fs=16000, wp=4000, param=8000, gpass=3, gstop=40, same_size=True):
+def LPF(new, fs=16000, wp=4000, param=8000, gpass=3, gstop=40, same_size=True, bits=16):
 
     assert torch.is_tensor(new) == True
     ori_shape = new.shape
@@ -42,6 +42,13 @@ def LPF(new, fs=16000, wp=4000, param=8000, gpass=3, gstop=40, same_size=True):
         new = new.squeeze(1) # (B, 1, T) --> (B, T)
     else:
         raise NotImplementedError('Audio Shape Error')
+    
+    if 0.9 * new.max() <= 1 and 0.9 * new.min() >= -1:
+        clip_max = 1
+        clip_min = -1
+    else:
+        clip_max = 2 ** (bits - 1) - 1
+        clip_min = -2 ** (bits - 1)
 
     ws = param
     wp = 2 * wp / fs
@@ -49,13 +56,20 @@ def LPF(new, fs=16000, wp=4000, param=8000, gpass=3, gstop=40, same_size=True):
     N, Wn = signal.buttord(wp, ws, gpass, gstop, analog=False, fs=None)
     b, a = signal.butter(N, Wn, btype='low', analog=False, output='ba')
     
-    audio = new.T.to("cpu")
-    a = torch.tensor(a, device="cpu", dtype=torch.float)
+    audio = new.T.to("cpu") # torch_lfilter only supports CPU tensor speed up
+    a = torch.tensor(a, device="cpu", dtype=torch.float) 
     b = torch.tensor(b, device="cpu", dtype=torch.float)
-    new_audio = lfilter(b, a, audio).T
+    new_audio = None
+    for ppp in range(audio.shape[1]): # torch_lfilter will give weird results for batch samples when using cpu tensor speed up; so we use naive loop here
+        new_audio_ = lfilter(b, a, audio[:, ppp:ppp+1]).T
+        if new_audio is None:
+            new_audio = new_audio_
+        else:
+            new_audio = torch.cat((new_audio, new_audio_), dim=0)
+    new_audio = new_audio.clamp(clip_min, clip_max)
     return new_audio.to(new.device).view(ori_shape)
 
-def BPF(new, fs=16000, wp=[300, 4000], param=[50, 5000], gpass=3, gstop=40, same_size=True):
+def BPF(new, fs=16000, wp=[300, 4000], param=[50, 5000], gpass=3, gstop=40, same_size=True, bits=16):
 
     assert torch.is_tensor(new) == True
     ori_shape = new.shape
@@ -68,6 +82,14 @@ def BPF(new, fs=16000, wp=[300, 4000], param=[50, 5000], gpass=3, gstop=40, same
     else:
         raise NotImplementedError('Audio Shape Error')
 
+    if 0.9 * new.max() <= 1 and 0.9 * new.min() >= -1:
+        clip_max = 1
+        clip_min = -1
+        # print(clip_max, clip_min)
+    else:
+        clip_max = 2 ** (bits - 1) - 1
+        clip_min = -2 ** (bits - 1)
+    
     ws = param
     wp = [2 * wp_ / fs for wp_ in wp]
     ws = [2 * ws_ / fs for ws_ in ws]
@@ -77,5 +99,14 @@ def BPF(new, fs=16000, wp=[300, 4000], param=[50, 5000], gpass=3, gstop=40, same
     audio = new.T.to("cpu")
     a = torch.tensor(a, device="cpu", dtype=torch.float)
     b = torch.tensor(b, device="cpu", dtype=torch.float)
-    new_audio = lfilter(b, a, audio).T
+    
+    new_audio = None
+    for ppp in range(audio.shape[1]):
+        new_audio_ = lfilter(b, a, audio[:, ppp:ppp+1]).T
+        if new_audio is None:
+            new_audio = new_audio_
+        else:
+            new_audio = torch.cat((new_audio, new_audio_), dim=0)
+    new_audio = new_audio.clamp(clip_min, clip_max)
+    
     return new_audio.to(new.device).view(ori_shape)
